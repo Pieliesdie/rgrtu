be811ec8d08c9a113fad2d1e2107b92064c27156
pieliesdie <Haketx@gmail.com>
2020-04-08 22:51:01 +0300

    cut

--- GIS2_3/GIS2/MainWindow.xaml.cs
+++ GIS2_3/GIS2/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,6 +17,7 @@ namespace GIS2
         Color256 = 256
     }
 
+
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -24,27 +27,43 @@ namespace GIS2
             Load(this.img2, "COLOR256.MTX", CountOfColors.Color256);
         }
 
-        void Load(Image img, string path, CountOfColors colors,bool invert=false)
+        void Load(Image img, string path, CountOfColors colors, bool invert = false)
         {
-            img.Source = ReadBmp(path, colors,invert);
+            var data = File.ReadAllBytes(path);
+            img.Source = ReadBmp(640, 480, data, colors, invert);
+        }
+
+        public static byte[] BitArrayToByteArray(BitArray bits)
+        {
+            byte[] ret = new byte[(bits.Length - 1) / 8 + 1];
+            bits.CopyTo(ret, 0);
+            return ret;
+        }
+
+        BitmapSource Cut(string path)
+        {
+            var data = File.ReadAllBytes(path);
+            List<byte> newdata = new List<byte>(data.Length);
+
+            for (int i = 100; i < 202; i++)
+            {
+                newdata.AddRange(data.ToList().Skip(320 * i + 269).Take(51));
+            }
+            return ReadBmp(102, 102, newdata.ToArray(), CountOfColors.Color16, false);
         }
 
         BitmapPalette InvertPalette(BitmapPalette src)
         {
             List<Color> colors = new List<Color>();
-      
+
             foreach (var i in src.Colors)
-                colors.Add( Color.FromRgb((byte)(255 - i.R), (byte)(255 - i.G), (byte)(255 - i.B)));
+                colors.Add(Color.FromRgb((byte)(255 - i.R), (byte)(255 - i.G), (byte)(255 - i.B)));
 
             return new BitmapPalette(colors);
         }
 
-        public BitmapSource ReadBmp(string Path, CountOfColors colorCount, bool invert = false)
+        public BitmapSource ReadBmp(int width, int height, byte[] data, CountOfColors colorCount, bool invert = false)
         {
-            var data = File.ReadAllBytes(Path);
-
-            int width = 640;
-            int height = 480;
             BitmapPalette palette;
             int stride;
             PixelFormat pixelFormat;
@@ -52,7 +71,7 @@ namespace GIS2
             switch ((int)colorCount)
             {
                 case 16:
-                    palette = invert==false? BitmapPalettes.Gray16:InvertPalette(BitmapPalettes.Gray16);
+                    palette = invert == false ? BitmapPalettes.Gray16 : InvertPalette(BitmapPalettes.Gray16);
                     stride = width / 2;
                     pixelFormat = PixelFormats.Indexed4;
                     break;
@@ -82,7 +101,7 @@ namespace GIS2
         private void Button_Click_1(object sender, RoutedEventArgs e) => SaveBMP(img2.Source as BitmapSource, "mtrx256.bmp");
 
 
-        void SaveBMP(BitmapSource img,string path)
+        void SaveBMP(BitmapSource img, string path)
         {
             using (var stream = new FileStream(path, FileMode.Create))
             {
@@ -92,6 +111,6 @@ namespace GIS2
             }
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e) => new ShowImg(new CroppedBitmap(img.Source as BitmapSource, new Int32Rect(520, 270, 101, 101))).Show();
+        private void Button_Click_2(object sender, RoutedEventArgs e) => new ShowImg(Cut("COLOR16.MTX")).Show();
     }
 }
