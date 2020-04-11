66f7de18b5a2b57d5e69a07905b752b942aade06
pieliesdie <Haketx@gmail.com>
2020-04-11 04:13:15 +0300

    Blerp

---
+++ GIS2_3/GIS2/CountOfColors.cs
@@ -0,0 +1,8 @@
+﻿namespace GIS2
+{
+    public enum CountOfColors
+    {
+        Color16 = 16,
+        Color256 = 256
+    }
+}
--- GIS2_3/GIS2/MainWindow.xaml.cs
+++ GIS2_3/GIS2/MainWindow.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -11,13 +9,6 @@ using System.Windows.Media.Imaging;
 
 namespace GIS2
 {
-    public enum CountOfColors
-    {
-        Color16 = 16,
-        Color256 = 256
-    }
-
-
     public partial class MainWindow : Window
     {
         byte[] data => File.ReadAllBytes("COLOR256.MTX");
@@ -27,20 +18,72 @@ namespace GIS2
             InitializeComponent();
             Load(this.img, "COLOR16.MTX", CountOfColors.Color16);
             Load(this.img2, "COLOR256.MTX", CountOfColors.Color256);
+
         }
 
-        void Load(Image img, string path, CountOfColors colors, bool invert = false)
+        void Load(Image img, string path, CountOfColors colors)
         {
             var data = File.ReadAllBytes(path);
-            img.Source = ReadBmp(640, 480, data, colors, invert);
+            img.Source = ReadBmp(640, 480, data, colors);
         }
 
-        public static byte[] BitArrayToByteArray(BitArray bits)
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-            byte[] ret = new byte[(bits.Length - 1) / 8 + 1];
-            bits.CopyTo(ret, 0);
-            return ret;
+            img2.Source = Scale(img2.Source as BitmapSource, double.Parse(scaleX.Text), double.Parse(scaleY.Text));
         }
+
+        private static double Lerp(double s, double e, double t)
+        {
+            return s + (e - s) * t;
+        }
+
+        private static double Blerp(double c00, double c10, double c01, double c11, double tx, double ty)
+        {
+            return Lerp(Lerp(c00, c10, tx), Lerp(c01, c11, tx), ty);
+        }
+
+        private static BitmapSource Scale(BitmapSource self, double scaleX, double scaleY)
+        {
+            if (self.Format != PixelFormats.Indexed8 && self.Palette != BitmapPalettes.Gray256)
+                throw new Exception("Unsupported");
+
+            int oldWidth = (int)self.Width;
+            int oldHeight = (int)self.Height;
+            byte[] old = new byte[oldWidth * oldHeight];
+            self.CopyPixels(old, oldWidth, 0);
+            var old2d = new D2<byte>(old, oldWidth);
+
+
+            int newWidth = (int)(oldWidth * scaleX);
+            int newHeight = (int)(oldHeight * scaleY);
+            byte[] _newimg = new byte[newHeight * newWidth];
+            var newimg = new D2<byte>(_newimg, newWidth);
+
+            for (int y = 0; y < newHeight; y++)
+            {
+                for (int x = 0; x < newWidth; x++)
+                {
+                    double gx = ((double)x) / newWidth * (oldWidth - 1);
+                    double gy = ((double)y) / newHeight * (oldHeight - 1);
+                    int gxi = (int)(gx);
+                    int gyi = (int)(gy);
+
+                    double c00 = old2d[gyi, gxi];
+                    double c10 = old2d[gyi, gxi + 1];
+                    double c01 = old2d[gyi + 1, gxi];
+                    double c11 = old2d[gyi + 1, gxi + 1];
+
+                    byte newColor = (byte)Blerp(c00, c10, c01, c11, gx - gxi, gy - gyi);
+                    newimg[y, x] = newColor;
+                }
+            }
+
+            var result = BitmapSource.Create(newWidth, newHeight, 96, 96, self.Format, self.Palette, newimg.input, newWidth);
+            result.Freeze();
+            return result;
+        }
+
 
         BitmapPalette ContrastPallete(BitmapPalette src, byte ymin, byte ymax)
         {
@@ -73,7 +116,7 @@ namespace GIS2
             {
                 newdata.AddRange(data.ToList().Skip(320 * i + 269).Take(51));
             }
-            return ReadBmp(102, 102, newdata.ToArray(), CountOfColors.Color16, false);
+            return ReadBmp(102, 102, newdata.ToArray(), CountOfColors.Color16);
         }
 
         BitmapPalette InvertPalette(BitmapPalette src)
@@ -86,7 +129,7 @@ namespace GIS2
             return new BitmapPalette(colors);
         }
 
-        public BitmapSource ReadBmp(int width, int height, byte[] data, CountOfColors colorCount, bool invert = false)
+        public BitmapSource ReadBmp(int width, int height, byte[] data, CountOfColors colorCount)
         {
             BitmapPalette palette;
             int stride;
@@ -95,12 +138,12 @@ namespace GIS2
             switch ((int)colorCount)
             {
                 case 16:
-                    palette = invert == false ? BitmapPalettes.Gray16 : InvertPalette(BitmapPalettes.Gray16);
+                    palette = BitmapPalettes.Gray16;
                     stride = width / 2;
                     pixelFormat = PixelFormats.Indexed4;
                     break;
                 case 256:
-                    palette = invert == false ? BitmapPalettes.Gray256 : InvertPalette(BitmapPalettes.Gray256);
+                    palette = BitmapPalettes.Gray256;
                     stride = width;
                     pixelFormat = PixelFormats.Indexed8;
                     break;
@@ -112,18 +155,9 @@ namespace GIS2
             return new TransformedBitmap(result, new ScaleTransform(1, -1));
         }
 
-        private void CheckBox_Checked(object sender, RoutedEventArgs e) => Load(this.img, "COLOR16.MTX", CountOfColors.Color16, true);
-
-        private void CheckBox_Checked_1(object sender, RoutedEventArgs e) => Load(this.img2, "COLOR256.MTX", CountOfColors.Color256, true);
-
-        private void CheckBox_Unchecked(object sender, RoutedEventArgs e) => Load(this.img, "COLOR16.MTX", CountOfColors.Color16, false);
-
-        private void CheckBox_Unchecked_1(object sender, RoutedEventArgs e) => Load(this.img2, "COLOR256.MTX", CountOfColors.Color256, false);
-
         private void Button_Click(object sender, RoutedEventArgs e) => SaveBMP(img.Source as BitmapSource, "mtrx16.bmp");
 
         private void Button_Click_1(object sender, RoutedEventArgs e) => SaveBMP(img2.Source as BitmapSource, "mtrx256.bmp");
-
 
         void SaveBMP(BitmapSource img, string path)
         {
@@ -138,10 +172,9 @@ namespace GIS2
         private void Button_Click_2(object sender, RoutedEventArgs e) => new ShowImg(Cut("COLOR16.MTX")).Show();
 
 
-
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            BitmapSource result = BitmapSource.Create(640, 480, 96, 96, PixelFormats.Indexed8, ContrastPallete(BitmapPalettes.Gray256,(byte)ymin.Value,(byte)ymax.Value), data, 640);
+            BitmapSource result = BitmapSource.Create(640, 480, 96, 96, PixelFormats.Indexed8, ContrastPallete(BitmapPalettes.Gray256, (byte)ymin.Value, (byte)ymax.Value), data, 640);
             result.Freeze();
             result = new TransformedBitmap(result, new ScaleTransform(1, -1));
             img2.Source = result;
